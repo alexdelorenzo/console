4c5827f093401ee5e0c7941cfc6f61ea5524d00d
Alex DeLorenzo <alex@alexdelorenzo.dev>
2023-01-18 21:21:06 -0500

    Don't include unrelated commits

--- python/packages/aws-lambda-sdk/serverless_lambda_sdk/tests/__init__.py
+++ python/packages/aws-lambda-sdk/serverless_lambda_sdk/tests/__init__.py
@@ -8,12 +8,6 @@ from typing import Callable, Dict
 from ..base import Handler
 
 
-environ["AWS_LAMBDA_FUNCTION_NAME"] = "example.handler"
-environ["SLS_ORG_ID"] = "hello.world"
-environ["AWS_LAMBDA_INITIALIZATION_TYPE"] = "on-demand"
-environ["AWS_LAMBDA_FUNCTION_VERSION"] = "1"
-
-
 Params = Dict[str, inspect.Parameter]
 
 
